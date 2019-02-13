ed1badb8ba76eced3eee8b331aa59bf0c9e399a8
Jose Roa <jg.roac@gmail.com>
2019-02-13 08:42:57 +0100

    feat: add epics for check and update the workload status automatically

--- src/state/workloads/__test___/epics.test.ts
+++ src/state/workloads/__test___/epics.test.ts
@@ -1,9 +1,12 @@
 import { TestScheduler } from 'rxjs/testing';
 import {
     createNewWorkload,
-    cancelWorkload
+    cancelWorkload,
+    checkWorkload,
+    updateWorkloadStatusAutomatically
 } from '../epics'
 import { of } from 'rxjs';
+import { ActionsObservable } from 'redux-observable';
 
 describe('createNewWorkload', () => {
     it(' should call the api to create a new workload and dispatch the create action', async () => {
@@ -96,5 +99,124 @@ describe('cancelWorkload', () => {
 
         const output$ = await cancelWorkload(action$, state$, dependencies).toPromise();
         expect(workloadServiceMock.cancel).toHaveBeenCalledTimes(0)
+    });
+})
+
+describe('checkWorkload', () => {
+    it(`should dispatch WORKLOAD_CHECK_STATUS some time after the workload was created
+        only if the workload status is 'WORKING'`, async () => {
+        const workloadMock = {
+            id: 1,
+            completeDate: '2018-01-01',
+            complexity: 5,
+            status: 'WORKING'
+        }
+        const input$ = of({
+            type: 'WORKLOAD_CREATED',
+            payload: workloadMock
+        });
+
+        const action$: any = new ActionsObservable(input$);
+        const state$: any = { value: { workloads: { 1: workloadMock } } };
+        const workloadServiceMock = {}
+
+        const dependencies = { workloadService: workloadServiceMock };
+
+        const output$ = await checkWorkload(action$, state$, dependencies).toPromise();
+        expect(output$).toEqual(
+            {
+                type: 'WORKLOAD_CHECK_STATUS',
+                payload: {
+                    id: workloadMock.id,
+                }
+            }
+        );
+    });
+
+    it(`shouldn't dispatch WORKLOAD_CHECK_STATUS is the workload status is 'WORKING'`, async () => {
+        const workloadMock = {
+            id: 1,
+            completeDate: '2018-01-01',
+            complexity: 5,
+            status: 'FAILURE'
+        }
+        const input$ = of({
+            type: 'WORKLOAD_CREATED',
+            payload: workloadMock
+        });
+
+        const action$: any = new ActionsObservable(input$);
+        const state$: any = { value: { workloads: { 1: workloadMock } } };
+        const workloadServiceMock = {}
+
+        const dependencies = { workloadService: workloadServiceMock };
+
+        const output$ = await checkWorkload(action$, state$, dependencies).toPromise();
+        expect(output$).toBeUndefined()
+    });
+})
+
+
+
+describe('updateWorkloadStatusAutomatically', () => {
+    it(`should check the workload status and update if it's neither 'WORKING' OR 'CANCELED'`, async () => {
+        const id = 2;
+        const action$: any = of({
+            type: 'WORKLOAD_CHECK_STATUS',
+            payload: { id }
+        });
+        const state$: any = null;
+        const workloadMock = {
+            id,
+            completeDate: '2018-01-01',
+            complexity: 5,
+            status: 'SUCCESS'
+        }
+        const workloadServiceMock = {
+            checkStatus: jest.fn().mockResolvedValue(workloadMock)
+        };
+
+        const dependencies = { workloadService: workloadServiceMock };
+
+        const output$ = await updateWorkloadStatusAutomatically(action$, state$, dependencies).toPromise();
+        expect(output$).toEqual(
+            {
+                type: 'WORKLOAD_UPDATE_STATUS',
+                payload: {
+                    id,
+                    status: workloadMock.status
+                }
+            }
+        );
+    });
+
+    it(`should dispatch a WORKLOAD_CHECK_STATUS action if the workload status is still 'WORKING'`, async () => {
+        const id = 2;
+        const action$: any = of({
+            type: 'WORKLOAD_CHECK_STATUS',
+            payload: { id }
+        });
+        const state$: any = null;
+        const workloadMock = {
+            id,
+            completeDate: '2018-01-01',
+            complexity: 5,
+            status: 'WORKING'
+        }
+        const workloadServiceMock = {
+            checkStatus: jest.fn().mockResolvedValue(workloadMock)
+        };
+
+        const dependencies = { workloadService: workloadServiceMock };
+
+        const output$ = await updateWorkloadStatusAutomatically(action$, state$, dependencies).toPromise();
+        expect(output$).toEqual(
+            {
+                type: 'WORKLOAD_CHECK_STATUS',
+                payload: {
+                    id
+                }
+            }
+        );
     });
 })
--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -1,29 +1,23 @@
 import { combineEpics, Epic, ofType } from 'redux-observable';
-import { map, tap, ignoreElements, filter, mergeMap, takeWhile } from 'rxjs/operators';
+import { map, tap, ignoreElements, filter, mergeMap, takeWhile, takeUntil, delay } from 'rxjs/operators';
+import moment from 'moment'
 
 // Types
 import { Action } from '../actions';
 import { State } from '../reducer';
 import {
   WORKLOAD_SUBMIT,
-  WORKLOAD_CANCEL
+  WORKLOAD_CANCEL,
+  WORKLOAD_CREATED,
+  WORKLOAD_CHECK_STATUS
 } from './types';
 import { WorkloadService } from './services';
 
 type AppEpic = Epic<Action, Action, State>;
 
 // Actions
-import { created, updateStatus } from './actions';
-
-
-const logWorkloadSubmissions: AppEpic = (action$, state$) => (
-  action$.pipe(
-    ofType('WORKLOAD_SUBMIT'),
-    map(action => action.payload),
-    tap((payload) => console.log('Workload submitted', payload)),
-    ignoreElements(),
-  )
-);
+import { created, updateStatus, checkStatus } from './actions';
+import { of } from 'rxjs';
 interface deps {
   workloadService: WorkloadService
 }
@@ -70,10 +64,75 @@ export const cancelWorkload: AppEpic = (
   )
 );
 
+export const checkWorkload: AppEpic = (
+  action$,
+  state$,
+) => (
+  action$.pipe(
+    filter(
+      (action: Action): action is WORKLOAD_CREATED => action.type === 'WORKLOAD_CREATED'
+    ),
+    map(action => action.payload.id),
+    mergeMap((payload) => {
+      const workload = state$.value.workloads[payload]
+      return of(workload).pipe(
+        takeUntil(
+          action$.ofType('WORKLOAD_CANCEL').pipe(
+            filter(
+              (a: Action) => {
+                const action = a as WORKLOAD_CANCEL;
+                return action.payload.id === workload.id;
+              }
+            )
+          )
+        ),
+        delay(moment(workload.completeDate).toDate()),
+        takeWhile((value) => {
+          const workload = state$.value.workloads[value.id];
+          return workload.status === 'WORKING';
+        }),
+        map((value) => checkStatus(value)),
+      )
+    }),
+  )
+);
+
+export const updateWorkloadStatusAutomatically: AppEpic = (
+  action$,
+  state$,
+  { workloadService }: deps
+) => (
+  action$.pipe(
+    filter(
+      (action: Action): action is WORKLOAD_CHECK_STATUS => action.type === 'WORKLOAD_CHECK_STATUS'
+    ),
+    map(action => action.payload),
+    mergeMap((value) => workloadService.checkStatus(value)),
+    mergeMap((workload) => {
+      if (workload.status === 'WORKING') {
+        return of(workload).pipe(delay(100));
+      }
+
+      return of(workload);
+    }),
+    map(workload => {
+      if (workload.status === 'WORKING') {
+        return checkStatus(workload);
+      }
+
+      return updateStatus({
+        id: workload.id,
+        status: workload.status
+      });
+    }),
+  )
+);
+
 export const epics = combineEpics(
-  logWorkloadSubmissions,
   createNewWorkload,
-  cancelWorkload
+  cancelWorkload,
+  checkWorkload,
+  updateWorkloadStatusAutomatically
 );
 
 export default epics;
