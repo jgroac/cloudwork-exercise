ef6f7f3e505997e379a3cae38911423adb3c73c9
Jose Roa <jg.roac@gmail.com>
2019-02-13 07:13:02 +0100

    test: add the provider and mock store to make the tests pass

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store: any = {
+    subscribe: jest.fn(),
+    dispatch: jest.fn(),
+    getState: jest.fn().mockReturnValue({ workloads: {} }),
+  }
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , div);
   ReactDOM.unmountComponentAtNode(div);
 });
